0a69421d9f7dc83a9d66935391c0172e9479b6b9
Felipe Hoyos <juancentral324@gmail.com>
2020-10-01 02:07:49 -0500

    ingreso rol cliente y mejora de las paginas

--- JS/nosotros.js
+++ JS/nosotros.js
@@ -1,43 +1,40 @@
 window.addEventListener("load",function(){
 
-    var hover_nav_principal = $(".contenedor_hipers");
+    var hover_nav_principal = $(".items");
+    var letras = $(".items>a");
+    var redes  = $(".items_redes>a");
     hover_nav_principal.hover(realizarcambio,devolvercambio);
+    letras.hover(realizarcambioletras,devolvercambioletras);
+    redes.hover(cambiocolorredes,devolvercolorredes);
+
+    function realizarcambioletras(){
+        $(this).css("color","#FF5733")
+    }
+    function devolvercambioletras(){
+        $(this).css("color","black")
+    }
+    function cambiocolorredes(){
+        $(this).css("color","#FF5733")
+    }
+    function devolvercolorredes(){
+        $(this).css("color","#fff");
+    }
+
 
     function realizarcambio(){
-        $(this).css("background","#FF5733")
-        .css("color","#fff")
-        .css("border-top","4px solid #7216af")
-        $(".hiper").css("text-decoration","none");
+        $(this).css("color","#FF5733")
+        .css("border-bottom","2px solid #7216af")
     }
     function devolvercambio(){
-        $(this).css("background","#fff")
-        .css("color","black")
-        .css("border-top","none"),
-        $(".hiper").css("color","black");
+        $(this).css("color","black")
+        .css("border-bottom","none")
     }
+    // Nav Responsive
+    var sliderResponsive = $(".hamburguesa");
+    var contenedor_responsive =$("#contenedor_principal_responsive");
 
-    // Evento para hover de afiliacion
-
-    var afiliacion = $("#hiper_afiliacion");
-
-    afiliacion.hover(colorafiliacion,coloresafilicion);
-
-    function colorafiliacion(){
-        var short_this = $(this);
-        short_this.css("background","black");
-    }
-    function coloresafilicion(){
-        var short_this = $(this);
-        short_this.css("background","#7216af");
-    }
-
-    // Slider para el buscador
-    var buscador = $(".icono");
-    var busca = $(".buscador");
-
-    busca.hide();
-
-    buscador.click(function(){
-    busca.slideToggle("fast");
+    contenedor_responsive.hide();
+    sliderResponsive.click(function(){
+        contenedor_responsive.slideToggle("fast")
     })
 })
